ae72dc3c4518e2cf618df74275daf1a91fb268bf
Mateus Cruz <mazaocruz2016@gmail.com>
2025-05-01 13:15:41 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GerenciamentoFuncionarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
